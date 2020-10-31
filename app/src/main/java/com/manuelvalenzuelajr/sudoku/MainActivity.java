f13a433e8f5f36e39f60ac82f96fb8f6e41cb4fe
Manu200162 <manli200134@gmail.com>
2020-10-31 17:17:06 -0400

    backtracking method

--- app/src/main/java/com/manuelvalenzuelajr/sudoku/MainActivity.java
+++ app/src/main/java/com/manuelvalenzuelajr/sudoku/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initViews();
         actions();
+
     }
     private void initViews(){
         resolveButton=findViewById(R.id.resolve);
@@ -116,9 +117,82 @@ public class MainActivity extends AppCompatActivity {
                      String s= casillas[i][j].getText().toString().trim();
                      int number= Integer.parseInt(s);
                      grid[i][j]=number;
+                     //casillas[i][j].setText(grid[][])
                  }
              }
          }
      });
   }
+    private boolean isSafe(int[][] board, int row, int col, int num) {
+        for (int d = 0; d < board.length; d++) {
+            if (board[row][d] == num) {
+                return false;
+            }
+        }
+
+        for (int r = 0; r < board.length; r++) {
+            if (board[r][col] == num) {
+                return false;
+            }
+        }
+
+        int sqrt = (int) Math.sqrt(board.length);
+        int boxRowStart = row - row % sqrt;
+        int boxColStart = col - col % sqrt;
+
+        for (int r = boxRowStart; r < boxRowStart + sqrt; r++) {
+            for (int d = boxColStart; d < boxColStart + sqrt; d++) {
+                if (board[r][d] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    private boolean solveSudoku(int[][] board, int n) {
+        int row = -1;
+        int col = -1;
+        boolean isEmpty = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) {
+                    row = i;
+                    col = j;
+                    isEmpty = false;
+                    break;
+                }
+            }
+            if (!isEmpty) {
+                break;
+            }
+        }
+        if (isEmpty) {
+            return true;
+        }
+        for (int num = 1; num <= n; num++) {
+            if (isSafe(board, row, col, num)) {
+                board[row][col] = num;
+                if (solveSudoku(board, n)) {
+                    return true;
+                } else {
+                    board[row][col] = 0;
+                }
+            }
+        }
+        return false;
+    }
+    private void print(int[][] board, int N) {
+        for (int r = 0; r < N; r++) {
+            for (int d = 0; d < N; d++) {
+                System.out.print(board[r][d]);
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+
+            if ((r + 1) % (int) Math.sqrt(N) == 0) {
+                System.out.print("");
+            }
+        }
+    }
 }
